a09afb218e0b12b3bc6d23d33fbe68b5299ee244
Joannis Orlandos <joannis@orlandos.nl>
2017-07-21 12:29:47 +0200

    Basic cookies tests

---
+++ Tests/LeopardTests/HeadersTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import Leopard
+
+class HeadersTests: XCTestCase {
+    func testBasics() throws {
+        var headers = Headers()
+        
+        headers["kaas"] = "meep"
+        XCTAssertEqual(headers["kaas"], "meep")
+        
+        headers["henk"] = "meep"
+        XCTAssertEqual(headers["henk"], "meep")
+        
+        headers["kaas"] = nil
+        XCTAssertEqual(headers["kaas"], nil)
+        XCTAssertEqual(headers["henk"], "meep")
+    }
+    
+    func testCookies() throws {
+        var cookies = Cookies()
+        cookies["kaas"] = "kaas"
+        cookies["sap"] = "sap"
+        cookies["saus"] = "saus"
+        
+        let request = Request(method: .get, url: "/")
+        request.cookies = cookies
+        
+        XCTAssertEqual(request.cookies["kaas"]?.value, "kaas")
+        XCTAssertEqual(request.cookies["sap"]?.value, "sap")
+        XCTAssertEqual(request.cookies["saus"]?.value, "saus")
+    }
+}
