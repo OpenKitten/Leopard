e293e8d4deda156f898231727d4e5f509627468c
Joannis Orlandos <joannis@orlandos.nl>
2017-07-24 11:06:13 +0200

    Depend on cheetah

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Leopard",
-            dependencies: ["Lynx", "Schrodinger"]),
+            dependencies: ["Lynx", "Schrodinger", "Cheetah"]),
         .testTarget(
             name: "LeopardTests",
             dependencies: ["Leopard", "Lynx", "Schrodinger"]),
